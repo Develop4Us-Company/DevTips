23de086524414f3b6b7d94f943761d27944043e2
Juarez Angelo dos Santos Júnior <juarez.a.s.junior@gmail.com>
2025-03-07 11:16:45 -0300

    StructBasico

---
+++ Portugues/CSharp/StructBasico/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+struct Ponto
+{
+    public int X;
+    public int Y;
+
+    public Ponto(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public void Exibir()
+    {
+        Console.WriteLine($"Ponto: ({X}, {Y})");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Ponto p = new Ponto(3, 5);
+        p.Exibir(); // Saída: Ponto: (3, 5)
+    }
+}

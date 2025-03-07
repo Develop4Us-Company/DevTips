24779b6d57c765cc883789d0d55c8f90aa1806ae
Juarez Angelo dos Santos Júnior <juarez.a.s.junior@gmail.com>
2025-03-07 14:24:59 -0300

    RefStructInterfaces

---
+++ Portugues/CSharp/RefStructInterfaces/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public interface IProcessamento
+{
+    void Processar();
+}
+
+public ref struct Processador : IProcessamento
+{
+    public void Processar()
+    {
+        Console.WriteLine("Processamento executado.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var processador = new Processador();
+        processador.Processar(); // Saída: Processamento executado.
+    }
+}

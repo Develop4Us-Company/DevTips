a8b9a2c0e5d7946693506b69115a02f309e63b91
Juarez Angelo dos Santos Júnior <juarez.a.s.junior@gmail.com>
2025-03-07 09:39:22 -0300

    AprimoramentosParams

---
+++ Portugues/CSharp/AprimoramentosParams/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Concat("Olá", "mundo", "!");
+Concat(1, 2, 3, 4, 5);
+
+static void Concat<T>(params ReadOnlySpan<T> items)
+{
+    foreach (var item in items)
+    {
+        Console.Write($"{item} ");
+    }
+    Console.WriteLine();
+}

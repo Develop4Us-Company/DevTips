e182e5deb594bcca61279d532c5a43b76043dbdd
Juarez Angelo dos Santos Júnior <juarez.a.s.junior@gmail.com>
2025-03-14 08:48:22 -0300

    PropriedadesParciais

---
+++ Portugues/CSharp/PropriedadesParciais/Program.cs
@@ -0,0 +1,33 @@
+﻿// Declaração da propriedade parcial
+public partial class Produto
+{
+    public partial decimal Preco { get; set; }
+}
+
+// Implementação da propriedade parcial
+public partial class Produto
+{
+    private decimal preco;
+
+    public partial decimal Preco
+    {
+        get => preco;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentOutOfRangeException(nameof(value), "O preço não pode ser negativo.");
+            preco = value;
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var produto = new Produto();
+        produto.Preco = 10;
+
+        Console.WriteLine("O preço do produto é: " + produto.Preco);
+    }
+}
